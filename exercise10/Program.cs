6674975e99e33cb70fce5da6349cf2e915cdbca1
badaev52pk1 <badaev52@gmail.com>
2022-11-26 15:17:42 +0300

    code ready exrercise10

---
+++ exercise10/Program.cs
@@ -0,0 +1,16 @@
+﻿string? str;
+int num;
+
+Console.Write("Введите число: ");
+str = Console.ReadLine();
+Console.WriteLine();
+
+num = Convert.ToInt32(str);
+
+num = num / 10;
+num = num % 10;
+
+// num = num % 100;
+// num = num / 10;
+
+Console.WriteLine(num);
