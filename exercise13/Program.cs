56ebe159d0949dc70ab62b61d0f324e71945ce52
badaev52pk1 <badaev52@gmail.com>
2022-11-26 15:33:32 +0300

    code ready exrercise13

---
+++ exercise13/Program.cs
@@ -0,0 +1,16 @@
+﻿string? str;
+int num;
+
+Console.Write("Введите число: ");
+str = Console.ReadLine();
+Console.WriteLine();
+
+num = Convert.ToInt32(str);
+
+num = num / 100;
+
+if (num > 0){    
+    Console.WriteLine("Третья цифра: " + num);
+}else{
+    Console.WriteLine("Третьей цифры нет!");
+}
