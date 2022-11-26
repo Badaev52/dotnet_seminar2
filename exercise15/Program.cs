e6ba6e360cdb0d30bf16cf4bd963c47202bf2bc1
badaev52pk1 <badaev52@gmail.com>
2022-11-26 15:41:10 +0300

    code ready exrercise15

---
+++ exercise15/Program.cs
@@ -0,0 +1,16 @@
+﻿string? str;
+int num;
+
+Console.Write("Введите число от 1 до 7: ");
+str = Console.ReadLine();
+Console.WriteLine();
+
+num = Convert.ToInt32(str);
+
+if(num > 0 && num < 8){
+
+    if(num == 6 || num == 7)    Console.WriteLine("ДА");
+    else                        Console.WriteLine("НЕТ");
+}else{
+    Console.WriteLine("Не правильно введенное число!");
+}
